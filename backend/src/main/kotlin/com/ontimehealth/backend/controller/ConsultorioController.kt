ea519b2ecabbd22e6efbf3006ec4c6b89e239ffb
guadalupehani <130237320+guadalupehani@users.noreply.github.com>
2026-06-09 11:20:06 -0300

    testing commit changes

--- backend/src/main/kotlin/com/ontimehealth/backend/controller/ConsultorioController.kt
+++ backend/src/main/kotlin/com/ontimehealth/backend/controller/ConsultorioController.kt
@@ -16,7 +16,7 @@ class ConsultorioController(private val consultorioRepository: ConsultorioReposi
                 "id" to it.id,
                 "nombre" to it.nombre,
                 "direccion" to it.direccion,
-                "telefono" to it.telefono
+                "telefono2" to it.telefono
             )
         }
         return ResponseEntity.ok(lista)
